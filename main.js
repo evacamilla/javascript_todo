b370be1faad4461b39ef62933b333f3627517164
cami <evacamilla.s@gmail.com>
2018-02-20 15:37:24 +0100

    finding elemets by id

---
+++ main.js
@@ -0,0 +1,10 @@
+//Button and input for making new todo
+const newTodoInput = document.getElementById('newTodoInput');
+const newTodoButton = document.getElementById('newTodoButton');
+
+//Buttons for deleting todos
+const deleteAllTodos = document.getElementById('deleteAllTodos');
+const deleteTodo = document.getElementById('deleteTodo');
+
+//Button for deleting completed todos
+const deleteCompleted = document.getElementById('deleteCompleted');
