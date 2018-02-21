cfda7880cf034f986921ce67a1bfc3965c392c78
cami <evacamilla.s@gmail.com>
2018-02-21 11:55:51 +0100

    replanned some things.. added buttons to each new list item in addNewTodo()

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 //Button for making new todo
 const newTodoButton = document.getElementById('newTodoButton');
 
-//Buttons for deleting todos
+//Button for deleting all todos
 const deleteAllTodos = document.getElementById('deleteAllTodos');
-const deleteTodo = document.getElementById('deleteTodo');
-
-//Button for deleting completed todos
-const deleteCompleted = document.getElementById('deleteCompleted');
 
 //ADD FUNCTIONS
 //add new todo
 function addNewTodo(){
-    newTodoInput.value
     var newLi = document.createElement('li');
     var newTodoInputValue = document.getElementById('newTodoInput').value;
-    //*TO MAKE*check if new todo is already existing or is empty
+    //*TO MAKE*check if value is empty string so user cant add empty string
+    //check if new to do is already existing
     var newTextNode = document.createTextNode(newTodoInputValue);
     newLi.appendChild(newTextNode);
+
+    var newDeleteButton = document.createElement('button');
+    newDeleteButton.innerText = 'w';
+
+    var newCompleteButton = document.createElement('button');
+    newDeleteButton.innerText = 'jw';
+
+    newLi.appendChild(newDeleteButton);
+    newLi.appendChild(newCompleteButton);
+
     document.getElementById('todoList').appendChild(newLi);
-    document.getElementById("newTodoInput").value = "Lägg till ny";
+    //value put to empty string so input field is empty when user clicked on add button
+    document.getElementById("newTodoInput").value = "";
 }
 
 
@@ -45,3 +52,10 @@ function addNewTodo(){
 
 //EVENTLISTENERS
 newTodoButton.addEventListener('click', addNewTodo);
+
+
+//EXTRAS
+/*
+- want newTodo to be first or last???
+- be able to put completed in Todos?
+*/
