c6f909f57f63557d64a65955d776e4ed8cdfe4af
cami <evacamilla.s@gmail.com>
2018-02-22 12:56:30 +0100

    started on storing and moving the data in arrays so that user should be able to see todo-list also after reloading/revisiting page.. in object data

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+//store data in object so that user can come back and find the todos
+var data = {
+    todo: [],
+    completed: []
+};
+
+
 //Getting buttons already in html
 const addTodoButton = document.getElementById('addTodoButton');
 const deleteAllTodos = document.getElementById('deleteAllTodos');
@@ -52,22 +59,46 @@ function deleteOneTodo(){
 
 
 
-//Actually toggles complete or not completed the todos
 function completeOrUncompleteTodo(){
     parentToMove = this.parentNode;
     parentToMoveFrom = parentToMove.parentNode;
+    //storing the value from the data.array so we can delete and store it in data object
+    var valueToMove = parentToMoveFrom.innerText;
         if(parentToMoveFrom.id === 'todoList')
             {
+            //delete from todo array and push to completed in data object
+            data.todo.splice(data.todo.indexOf(valueToMove, 1));
+            data.completed.push(valueToMove);
             document.getElementById('completedTodoList').appendChild(parentToMove);
             this.classList.remove('uncompleted');
             this.classList.add('completed');
             } else {
+                //delete from completed array and push to todo in data object
+                data.completed.splice(data.completed.indexOf(valueToMove, 1));
+                data.todo.push(valueToMove);
                 document.getElementById('todoList').appendChild(parentToMove);
                 this.classList.remove('completed');
                 this.classList.add('uncompleted');
                     }
-    
+    console.log(data);
 }
+
+
+//EVENTLISTENERS
+//*TO MAKE*so user can't make the same todo twice, maybe ask
+addTodoButton.addEventListener('click', function(){
+    var addTodoInputValue = document.getElementById('addTodoInput').value;
+    //if user try to add empty string show messge
+    if(addTodoInputValue == ""){
+                            document.getElementById('emptyStringMessage').style.display = "block";
+                                }
+    else{
+        document.getElementById('emptyStringMessage').style.display = "none";
+        //stores added string in todo array so user can see it also after reloading the page
+        data.todo.push(addTodoInputValue);
+        addNewTodo(addTodoInputValue);
+        }
+});
 
 
 
@@ -93,23 +124,15 @@ function completeOrUncompleteTodo(){
 
 
 
-//EVENTLISTENERS
-//*TO MAKE*so user can't make the same todo twice, maybe ask
-addTodoButton.addEventListener('click', function(){
-    var addTodoInputValue = document.getElementById('addTodoInput').value;
-    //so user cant add empty string. something more should happen though..?
-    if(addTodoInputValue == ""){
-    }
-    else{
-    addNewTodo(addTodoInputValue);}
-});
-
-
-
+/**TO DO **/
+/*
+new todo on top
+erase all todos
+*/
 
 
 //EXTRAS
 /*
-- want newTodo to be first or last???
-- be able to put completed back in Todos.. or only delete?
+transition/animate when completed
+press enter to add
 */
